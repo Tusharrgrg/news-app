c4c95d1120417049e60b9f2a6b84e57d42d2bdd4
Tushar agrawal <gargtushar495@gmail.com>
2023-03-15 18:31:45 +0530

    add spinner and bug fix

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,27 +1,28 @@
 import React, { Component } from 'react'
+import {Link} from "react-router-dom";
+  
 
 export class NavBar extends Component {
     render() {
         return (
             <div>
-                <nav className="navbar navbar-expand-md navbar-light bg-light">
-                    <div className="container">
-                        <a className="navbar-brand" href="index.html">NewsZzy</a>
-                        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample05" aria-controls="navbarsExample05" aria-expanded="false" aria-label="Toggle navigation">
+                <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                    <div className="container-fluid">
+                        <Link className="navbar-brand" to="index.html">NewsZzy</Link>
+                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon"></span>
                         </button>
 
-                        <div className="collapse navbar-collapse" id="navbarsExample05">
-                            <ul className="navbar-nav ml-auto pl-lg-5 pl-0">
-                                <li className="nav-item">
-                                    <a className="nav-link active" href="index.html">Home</a>
-                                </li>
-                                <li className="nav-item">
-                                    <a className="nav-link" href="/about">About</a>
-                                </li>
-                                {/* <li className="nav-item">
-                                    <a className="nav-link" href="/">News</a>
-                                </li> */}
+                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                                <li className="nav-item"><Link className="nav-link" aria-current="page"to="/">Home</Link></li>
+                                <li className="nav-item"><Link className="nav-link" to="/business">Business</Link></li>
+                                <li className="nav-item"><Link className="nav-link" to="/entertainment">Entertainment</Link></li>
+                                <li className="nav-item"><Link className="nav-link" to="/general">General</Link></li>
+                                <li className="nav-item"><Link className="nav-link" to="/health">Health</Link></li>
+                                <li className="nav-item"><Link className="nav-link" to="/science">Science</Link></li>
+                                <li className="nav-item"><Link className="nav-link" to="/sports">Sports</Link></li>
+                                <li className="nav-item"><Link className="nav-link" to="/technology">Technology</Link></li>
                             </ul>
                         </div>
                     </div>
--- src/components/News.js
+++ src/components/News.js
@@ -1,65 +1,96 @@
 import React, { Component } from 'react'
 import NewsItem from './NewsItem'
+import Spinner from './Spinner';
+import PropTypes from 'prop-types'
+import InfiniteScroll from "react-infinite-scroll-component";
+
+
 
 export class News extends Component {
+  static defaultProps = {
+    country: "in",
+    pageSize: 7,
+    category: "general"
+  }
 
-  articles = [
-    {
-      "source": {
-        "id": "news24",
-        "name": "News24"
-      },
-      "author": "Heinz Schenk",
-      "title": "Proteas quickie Coetzee learns from pesky last-wicket stand: 'Luckily we still have a good lead'",
-      "description": "Much might've gone right over the past week or so for Gerald Coetzee after his introduction to Test cricket, but the rookie quick also had to contend with the \"pain\" of the classic pesky last-wicket stand.",
-      "url": "https://www.news24.com/sport/cricket/proteas/proteas-quickie-coetzee-learns-from-pesky-last-wicket-stand-luckily-we-still-have-a-good-lead-20230309",
-      "urlToImage": "https://cdn.24.co.za/files/Cms/General/d/8519/976a939b0617427fa840b4968c2f7738.jpg",
-      "publishedAt": "2023-03-09T20:01:58+00:00",
-      "content": "At the Wanderers\r\n<ul><li>Young Gerald Coetzee ended a successful day by also experiencing his first pesky last-wicket stand with the Proteas.</li><li>The alliance between Jason Holder and Gudakesh M… [+3786 chars]"
-    },
-    {
-      "source": {
-        "id": "abc-news-au",
-        "name": "ABC News (AU)"
-      },
-      "author": "ABC News",
-      "title": "Live scorecard: India vs Australia, fourth Test in Ahmedabad",
-      "description": "Australia is looking to draw the series with victory in cricket's largest stadium, the Narendra Modi Stadium in Ahmedabad. Follow all the live scores in our scorecard.",
-      "url": "http://www.abc.net.au/news/2023-03-09/live-scorecard-india-vs-australia-fourth-test-ahmedabad/102068776",
-      "urlToImage": "https://live-production.wcms.abc-cdn.net.au/efe80d41a60494b6829cf55bc923150f?impolicy=wcms_crop_resize&cropH=2813&cropW=5000&xPos=0&yPos=431&width=862&height=485",
-      "publishedAt": "2023-03-09T03:33:13Z",
-      "content": "There is a watch and act warning in place for Tambaroora in New South Wales. For the latest information, search ABC Emergency"
-    },
-  ]
+  static propTypes = {
+    country: PropTypes.string,
+    pageSize: PropTypes.number,
+    category: PropTypes.string
+  }
 
-
-
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
-      articles: this.articles,
-      loading: false
+      articles: [],
+      loading: false,
+      page: 1,
+      totalResults:0
     };
   }
 
-  async componentDidMount() {
-    let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=afe813c65ea940b5849bd478a02327b9";
-    let data = await fetch(url);
-    let parseData = await data.json(data);
-    this.setState({ articles: parseData.articles });
+  updateNews = async () => {
+    this.props.setProgress(0);
+    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+    this.setState({
+      loading: true,
+    });
+    await fetch(url).then(res => res.json()).then(response => {
+      this.setState({
+        loading: false,
+        articles: response.articles,
+        totalResults: response.totalResults
+      });
+    });
+
+    this.props.setProgress(100);
   }
+
+  async componentDidMount() {
+    this.updateNews();
+  }
+
+  fetchMoreData = async() => {
+    this.setState({
+      page:this.state.page+1
+    });
+
+    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page+1}&pageSize=${this.props.pageSize}`;
+    this.setState({
+      loading: true,
+    });
+    await fetch(url).then(res => res.json()).then(response => {
+      this.setState({
+        loading: false,
+        articles:  this.state.articles.concat(response.articles),
+        totalResults: response.totalResults,
+      });
+    });
+  };
 
   render() {
     return (
-      <div className='container'>
-        <div className="row my-4">
-          {this.state.articles.map((element) => {
-            return <div className="col col-md-4 my-3" key={element.url}>
-              <NewsItem title={element.title ? element.title.slice(0, 40) : ""} description={element.description ? element.description.slice(0, 85) : ""} imgUrl={element.urlToImage ? element.urlToImage : "https://images.unsplash.com/photo-1602922960044-d48ce791d3c0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=865&q=80"} newsUrl={element.url} />
+      <>
+        <h1 className='text-center' style={{margin:'35px 0px'}}>NewZzy - Top {this.props.category} Headlines</h1>
+        {this.state.loading && <Spinner />}
+
+        <InfiniteScroll
+          dataLength={this.state.articles.length}
+          next={this.fetchMoreData}
+          hasMore={this.state.articles.length !== this.state.totalResults}
+          loader={<Spinner />}
+        >
+          <div>
+            <div className="row ">
+              {this.state.articles.map((element,index) => {
+                return <div className="my-3" key={index}>
+                  <NewsItem title={element.title ? element.title : ""} description={element.description ? element.description : ""} imgUrl={element.urlToImage ? element.urlToImage : "https://images.unsplash.com/photo-1602922960044-d48ce791d3c0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=865&q=80"} newsUrl={element.url} author={element.author} date={element.publishedAt} />
+                </div>
+              })}
             </div>
-          })}
-        </div>
-      </div>
+          </div>
+        </InfiniteScroll>
+      </>
     )
   }
 }
